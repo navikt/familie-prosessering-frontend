55e3b024846bc77a288d2c55f65a5d0f59159a52
charliemidtlyng <charlie.midtlyng@bekk.no>
2025-01-09 15:23:49 +0100

    Ny react-versjon og eldgammel hot loader fungerte ikke å kjøre lokalt. I tillegg er ts-loader utdatert. Byttet til babel-loader og react-refresh

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
-import { AppContainer } from 'react-hot-loader';
 import App from './komponenter/App';
 import '@navikt/ds-tokens';
 
@@ -9,18 +8,4 @@ import './index.less';
 const rootElement = document.getElementById('app');
 const root = createRoot(rootElement!);
 
-const renderApp = (Component: React.ComponentType): void => {
-    root.render(
-        <AppContainer>
-            <Component />
-        </AppContainer>
-    );
-};
-
-renderApp(App);
-
-if (module.hot) {
-    module.hot.accept('./komponenter/App', () => {
-        renderApp(App);
-    });
-}
+root.render(<App />);
